a00f7ab1292d469e6e8de6c30546fb09f5d72524
Wladimir <Fast126@mail.ru>
2020-07-07 16:28:44 +0300

    added a  couple of lines to readme

--- README.md
+++ README.md
@@ -1,2 +1,19 @@
 # iikoTransport
-библиотека для работы с  iikoTransport 
+Библиотека для работы с  iikoTransport 
+
+
+## Install 
+
+## Get Started
+```
+$transport = new IikoTransport('api key'); 
+```
+
+
+## Organization 
+
+### Returns organizations available to api-login user. 
+```
+$response = $transport->getOrganizationManager()->getList();
+echo ($response->toArray()); 
+```
--- src/IikoTransport.php
+++ src/IikoTransport.php
@@ -1,6 +1,10 @@
 <?php 
 namespace IikoTransport; 
 
+
+use IikoTransport\Client; 
+use IikoTransport\OrganizationManager; 
+
 class IikoTransport 
 {
 
@@ -8,8 +12,19 @@ class IikoTransport
    * @var Client
    */
   private $client; 
+  /**
+   * @var OrganizationManager
+   */
+  private $organizationManager; 
 
   public function __construct(string $api_key) {
+    $this->client= new Client($api_key); 
+  }
 
+  public function getOrganizationManager():OrganizationManager {
+    if (!$this->organizationManager){
+      $this->organizationManager = new OrganizationManager($this->client); 
+    }
+    return $this->organizationManager; 
   }
 }
